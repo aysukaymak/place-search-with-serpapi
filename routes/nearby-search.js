a4ba8a7a15df6a3d7cc9f39cfb53087be75c28d3
Aysu Kaymak <aysukaymak50@gmail.com>
2024-06-06 15:40:06 +0300

    nearby-search is completed

--- routes/nearby-search.js
+++ routes/nearby-search.js
@@ -1,32 +1,35 @@
 import { getJson } from "serpapi";
-import dotenv from 'dotenv'
+import dotenv from 'dotenv';
 
-dotenv.config(); // Load environment variables
+dotenv.config();
 
-// search places with keywords and location
+const apiKey = `${process.env.SERPAPI_API_KEY}`;
+
 async function searchPlaces(latlong) {
     try {
-        console.log(process.env.SERPAPI_API_KEY)
-        getJson({
+        const params = {
             engine: "google_maps",
             q: "pizza",
-            ll: `${latlong},15.1z`,
+            ll: `@${latlong},15.1z`,
             type: "search",
-            api_key: "c8ee9205d74b180c569192152996a97e89b34333ecbfd52556b325edfcce1f5a"
-        }, (json) => {
-            console.log(json["local_results"]);
-        });
+            api_key: apiKey
+        };
+        const json = await getJson(params);
+        console.log(params);        
+        console.log(json.local_results);
+        return json.local_results;
     } catch (error) {
-        console.log("Error searching places:", error);
+        console.error("Error searching places:", error);
         throw error;
     }
 }
 
 async function nearbySearch(latlong) {
     try {
-        await searchPlaces(latlong);
+        return await searchPlaces(latlong);
     } catch (error) {
-        console.log("Error in main function:", error);
+        console.error("Error in nearbySearch function:", error);
+        throw error;
     }
 }
 
